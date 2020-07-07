594198d032da77d3c5822e38b34c472862125da9
unknown <avi1999kokare@gmail.com>
2020-07-07 14:14:20 +0530

    Updates

---
+++ precache-manifest.7a0ded8c9277390531076286ee61da8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3ca2bd9863b83dedac892da8283c1d",
+    "url": "/LoginPage/index.html"
+  },
+  {
+    "revision": "da5ef41220db02352202",
+    "url": "/LoginPage/static/css/main.dd6fade0.chunk.css"
+  },
+  {
+    "revision": "5a45296b93def30abac9",
+    "url": "/LoginPage/static/js/2.7710542a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LoginPage/static/js/2.7710542a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5ef41220db02352202",
+    "url": "/LoginPage/static/js/main.7d6e209d.chunk.js"
+  },
+  {
+    "revision": "669eec3c4da36b29838a",
+    "url": "/LoginPage/static/js/runtime-main.ebb24892.js"
+  }
+]);
